aa5805387d9357798a11628d3a34d5c73f824d09
Денис Корчагин <SniperNewMeta@yandex.ru>
2024-08-15 11:30:08 +0300

    Add access filter

---
+++ Departments.Service_A/Filters/AccessFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.RegularExpressions;
+
+namespace Departments.Service_A.Filters
+{
+    public class AccessFilterAttribute : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            string userAgent = context.HttpContext.Request.Headers["Access-Hash"].ToString();
+        }
+    }
+}
